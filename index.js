f5dbf67e746d10fc632bf66c17c6b8c050943a78
smahmud <smahmud080@gmail.com>
2024-12-30 23:53:00 +0600

    spining add

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const allPetCategory = async() =>{
     const data = await response.json();
     petBtn(data.categories);
 }
-// Fetch All Pets
-const allPets = async () =>{
-   const response = await fetch (`https://openapi.programming-hero.com/api/peddy/pets`)
+const categoryId = async () =>{
+   const response = await fetch(`https://openapi.programming-hero.com/api/peddy/category/dog`)
    const data = await response.json();
-   console.log(data.pets);
-
+   console.log(data.data);
 }
+categoryId()
 
 const petBtn = (buttons) => {
+   document.getElementById("spinner").style.display = "none"
    const btnContainer = document.getElementById('pet-btn');
+btnContainer.innerHTML = '';
 // console.log(buttons);
 buttons.map((button) =>{
 // console.log(button);
@@ -21,7 +22,7 @@ const { category_icon, category } = button;
 // console.log(category_icon);
    const createButton = document.createElement('div')
 createButton.innerHTML = `
- <button class="flex items-center justify-center gap-2 p-2 rounded-3xl border bg-stone-100  border-green-300 w-28">
+ <button onclick="clickSpiner('${category}')" class="flex items-center justify-center gap-2 p-2 rounded-3xl border bg-stone-100  border-green-300 w-28">
                   <img src="${category_icon}" alt="Cat" class="w-6 h-6" />
                   <span class="font-semibold">${category}</span>
                 </button>
@@ -30,5 +31,31 @@ btnContainer.append(createButton)
 });
 }
 
+
+
+
+const clickSpiner = () => {
+
+   document.getElementById("spinner").style.display = "block"
+
+
+   setTimeout(() => {
+      allPetCategory();
+   }, 2000);
+}
+
+// Fetch All Pets
+const allPets = async () =>{
+   const response = await fetch (`https://openapi.programming-hero.com/api/peddy/pets`)
+   const data = await response.json();
+   loadAllPets(data.pets);
+
+}
+const loadAllPets = (allPets) =>{
+allPets.map((pet) => {
+
+})
+}
+allPets()
 
 allPetCategory()
